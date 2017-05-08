36f229725d4ffa67c3fbbc2d0578a8df9a0278d7
Zac McClung <zac.mcclung370@gmail.com>
2017-05-07 20:43:02 -0600

    edit server.js

--- server.js
+++ server.js
@@ -6,37 +6,30 @@ const configs = require('./config.js');
 
 
 var app = module.exports = express();
-// var connectionString = 'postgres://postgres:@localhost/tesla'
+
 // var massiveInstance = massive.connectSync({connectionString : connectionString}),
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(cors());
 
-var db = massive.connectSync({connectionString : configs.serverSecret})
+var connectionString = 'postgres://tesladb1:password@localhost/tesla'
+var db = massive.connectSync({connectionString : connectionString})
 app.set('db', db);
 var db =  app.get('db')
-
-//   function(err, localdb){
-    // app.run(function(){
-      // db.createTables(function(x){
-      //   console.log("!!!!!!!!!! Tables Created !!!!!!!!!!");
-      //   // console.log(x);
-      // });
-    // })
-  // }
-// )
-
-
 
 
 
 
 var serverCtrl = require('./serverCtrl.js');
 
-const port = 4040;
+const port = 80;
 app.listen(port, function(){
   console.log("Yoda Lay Hee Whoo", port)
+  // db.createTables(function(x){
+  //    console.log("!!!!!!!!!! Tables Created !!!!!!!!!!");
+  //     console.log(x);
+  //  });
 })
 
 app.get('/api/cars', serverCtrl.getAllCars)
