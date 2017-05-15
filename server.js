2adbefd6141f03d7faa6b02c542390ab6cc5f291
root <zac.mcclung370@gmail.com>
2017-05-15 17:21:28 +0000

    uncommented create tables function

--- server.js
+++ server.js
@@ -60,10 +60,10 @@ var serverCtrl = require('./serverCtrl.js');
 const port = 80;
 app.listen(port, function(){
   console.log("Yoda Lay Hee Whoo", port)
-  // db.createTables(function(x){
-  //    console.log("!!!!!!!!!! Tables Created !!!!!!!!!!");
-  //     console.log(x);
-  //  });
+   db.createTables(function(x){
+     console.log("!!!!!!!!!! Tables Created !!!!!!!!!!");
+       console.log(x);
+  });
 })
 
 app.get('/api/cars', serverCtrl.getAllCars)
